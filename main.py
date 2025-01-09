b4647da44af0661c41a5482b505934d03e332bdc
foggystar <fdgaojx@126.com>
2025-01-09 19:41:45 +0800

    Remove deleted files

--- main.py
+++
@@ -1,6 +0,0 @@
-# Please install OpenAI SDK first: `pip3 install openai`
-
-import get_result
-
-
-print(get_result.judge_result())
--- util.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def getDeepSeekKey():
-    return os.getenv('DEEPSEEK_API_KEY', '')
-
-def getDeepSeekUrl():
-    return "https://api.deepseek.com"
